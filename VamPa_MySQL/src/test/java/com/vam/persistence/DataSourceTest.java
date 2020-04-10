4b25f92c1fc211869c53c696aac61c58f7f012b8
SeoJin Kim <sjinjin7@gmail.com>
2020-04-10 11:50:38 +0900

    MyBatis 라이브러리 추가 - pom.xmldp myabatis/ spring-mybatis / spring-tx / spring-jdbc 추가 - root-context.xml에서 SqlSessionFactoryBean 추가 - 추가된 라이브러리 및 context 설정 테스트를 DataSourcetest.java에서 실행

--- VamPa_MySQL/src/test/java/com/vam/persistence/DataSourceTest.java
+++ VamPa_MySQL/src/test/java/com/vam/persistence/DataSourceTest.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +19,22 @@ public class DataSourceTest {
 	@Autowired
 	private DataSource dataSource;
 	
+	@Autowired
+	private SqlSessionFactory sqlSessionFactory;
+	
 	@Test
 	public void testConnection() {
 		try(
-			Connection con = dataSource.getConnection();	
-			){
+			Connection con = dataSource.getConnection();
+			SqlSession session = sqlSessionFactory.openSession();
+		){
 			
-			System.out.println("con="+con);
+		System.out.println("con=" + con);
+		System.out.println("session=" + session);
 			
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
